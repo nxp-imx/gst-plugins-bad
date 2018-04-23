e7df9699b486148146a98c4902e867f92e44d4fa
Haihua Hu <jared.hu@nxp.com>
2018-04-23 12:38:38 +0800

    MMFMWK-7954 waylandsink: pass dmabuf modifier to weston
    upstream status: imx specific
    
    (cherry picked from commit 78d9555078c83b1b09bc603b9bbd05c9313d2611)

--- ext/wayland/gstwaylandsink.c
+++ ext/wayland/gstwaylandsink.c
@@ -51,6 +51,9 @@
 #include <gst/video/videooverlay.h>
 #include <linux/version.h>
 
+#include <xf86drm.h>
+#include <xf86drmMode.h>
+
 #include "hdr10-metadata-unstable-v1-client-protocol.h"
 
 /* signals */
@@ -847,6 +850,7 @@ gst_wayland_sink_propose_allocation (GstBaseSink * bsink, GstQuery * query)
   GstBufferPool *pool = NULL;
   gboolean need_pool;
   GstAllocator *alloc;
+  guint64 drm_modifier;
   GstVideoInfoDmaDrm drm_info;
   GstVideoInfo vinfo;
   guint size;
@@ -866,6 +870,9 @@ gst_wayland_sink_propose_allocation (GstBaseSink * bsink, GstQuery * query)
       return FALSE;
     size = vinfo.size;
   }
+
+  drm_modifier = DRM_FORMAT_MOD_AMPHION_TILED;
+  gst_query_add_allocation_dmabuf_meta (query, drm_modifier);
 
   if (need_pool) {
     GstStructure *config;
