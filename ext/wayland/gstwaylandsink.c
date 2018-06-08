d0c95d32a310985294c01415513287335d8e7282
Haihua Hu <jared.hu@nxp.com>
2018-06-08 16:52:44 +0800

    MMFMWK-8030 waylandsink: support video crop meta using viewporter protocol
    use API wp_viewport_set_source() to support video crop meta handle
    
    upstream status: Pending
    https://bugzilla.gnome.org/show_bug.cgi?id=796541

--- ext/wayland/gstwaylandsink.c
+++ ext/wayland/gstwaylandsink.c
@@ -817,6 +817,7 @@ gst_wayland_sink_propose_allocation (GstBaseSink * bsink, GstQuery * query)
   }
 
   gst_query_add_allocation_meta (query, GST_VIDEO_META_API_TYPE, NULL);
+  gst_query_add_allocation_meta (query, GST_VIDEO_CROP_META_API_TYPE, NULL);
 
   return TRUE;
 }
@@ -883,6 +884,8 @@ gst_wayland_sink_show_frame (GstVideoSink * vsink, GstBuffer * buffer)
   /* make sure that the application has called set_render_rectangle() */
   if (G_UNLIKELY (gst_wl_window_get_render_rectangle (self->window)->w == 0))
     goto no_window_size;
+
+  gst_wl_window_set_source_crop (self->window, buffer);
 
   wlbuffer = gst_buffer_get_wl_buffer (self->display, buffer);
 
