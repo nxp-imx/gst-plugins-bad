4417ca49193b7fb7910ee71cab1c4c9b191c1d69
Haihua Hu <jared.hu@nxp.com>
2020-04-20 16:02:55 +0800

    MMFMWK-8759 waylandsink: chose the first display size
    when enable multi display, need to chose the first display
    size as the default full screen size.
    
    upstream status: imx specific

--- gst-libs/gst/wayland/gstwldisplay.c
+++ gst-libs/gst/wayland/gstwldisplay.c
@@ -55,13 +55,13 @@ typedef struct _GstWlDisplayPrivate
   struct zwp_fullscreen_shell_v1 *fullscreen_shell;
   struct wp_single_pixel_buffer_manager_v1 *single_pixel_buffer;
   struct wl_shm *shm;
-  struct wl_output *output;
   struct wp_viewporter *viewporter;
   struct zwp_linux_dmabuf_v1 *dmabuf;
   struct zwp_alpha_compositing_v1 *alpha_compositing;
   GArray *shm_formats;
   GArray *dmabuf_formats;
   GArray *dmabuf_modifiers;
+  GArray *outputs;
 
   /* real display resolution */
   gint width, height;
@@ -104,6 +104,7 @@ gst_wl_display_init (GstWlDisplay * self)
   priv->shm_formats = g_array_new (FALSE, FALSE, sizeof (uint32_t));
   priv->dmabuf_formats = g_array_new (FALSE, FALSE, sizeof (uint32_t));
   priv->dmabuf_modifiers = g_array_new (FALSE, FALSE, sizeof (guint64));
+  priv->outputs = g_array_new (FALSE, FALSE, sizeof (struct wl_output *));
   priv->wl_fd_poll = gst_poll_new (TRUE);
   priv->buffers = g_hash_table_new (g_direct_hash, g_direct_equal);
   priv->width = -1;
@@ -128,6 +129,8 @@ gst_wl_ref_wl_buffer (gpointer key, gpointer value, gpointer user_data)
 static void
 gst_wl_display_finalize (GObject * gobject)
 {
+  guint i;
+  struct wl_output *output = NULL;
   GstWlDisplay *self = GST_WL_DISPLAY (gobject);
   GstWlDisplayPrivate *priv = gst_wl_display_get_instance_private (self);
 
@@ -189,6 +192,15 @@ gst_wl_display_finalize (GObject * gobject)
 
   if (priv->subcompositor)
     wl_subcompositor_destroy (priv->subcompositor);
+
+  if (priv->outputs) {
+    for (i = 0; i < priv->outputs->len; i++) {
+      output = g_array_index (priv->outputs, struct wl_output *, i);
+      if (output)
+        wl_output_destroy (output);
+    }
+    g_array_unref (priv->outputs);
+  }
 
   if (priv->registry)
     wl_registry_destroy (priv->registry);
@@ -354,6 +366,7 @@ static const struct xdg_wm_base_listener xdg_wm_base_listener = {
   handle_xdg_wm_base_ping
 };
 
+static void
 output_handle_geometry (void *data, struct wl_output *wl_output,
     int32_t x, int32_t y,
     int32_t physical_width, int32_t physical_height,
@@ -372,8 +385,11 @@ output_handle_mode (void *data, struct wl_output *wl_output,
 
   /* we only care about the current mode */
   if (flags & WL_OUTPUT_MODE_CURRENT) {
-    priv->width = width;
-    priv->height = height;
+    if (priv->width == -1 && priv->height == -1) {
+      priv->width = width;
+      priv->height = height;
+    }
+
   }
 }
 
@@ -439,9 +455,11 @@ registry_handle_global (void *data, struct wl_registry *registry,
     priv->alpha_compositing =
         wl_registry_bind (registry, id, &zwp_alpha_compositing_v1_interface, 1);
   } else if (g_strcmp0 (interface, "wl_output") == 0) {
-    priv->output =
+    struct wl_output *output;
+    output =
 	wl_registry_bind (registry, id, &wl_output_interface, MIN (version, 2));
-    wl_output_add_listener (priv->output, &output_listener, self);
+    wl_output_add_listener (output, &output_listener, self);
+    g_array_append_val (priv->outputs, output);
   }
 }
 
