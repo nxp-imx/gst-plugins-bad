477abf1de7b78896d96b7e64143a8e97053066b7
Haihua Hu <jared.hu@nxp.com>
2020-12-17 10:03:06 +0800

    play: support set connection-speed property of playbin
    Add an API to support set connection-speed property of playbin
    
    upstream status: pending
    https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/merge_requests/1889
    
    (cherry picked from commit 7c71a57d356750451b28047ddc71e23f8adfd37c)

--- gst-libs/gst/play/gstplay.h
+++ gst-libs/gst/play/gstplay.h
@@ -345,6 +345,10 @@ gboolean     gst_play_set_track_ids                 (GstPlay     *play,
                                                      const gchar *subtitle_stream_id);
 
 GST_PLAY_API
+gboolean     gst_play_set_connection_speed          (GstPlay    *play,
+                                                     guint connnection_speed);
+
+GST_PLAY_API
 GstPlayMediaInfo *    gst_play_get_media_info     (GstPlay * play) G_GNUC_WARN_UNUSED_RESULT;
 
 GST_PLAY_API
