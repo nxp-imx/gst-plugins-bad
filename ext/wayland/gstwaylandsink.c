b75e1b38aa3c54f7fa048c53e1cc02dea476d7f6
Haihua Hu <jared.hu@nxp.com>
2025-06-20 15:00:00 +0900

    LF-15272 waylandsink: don't always add dmabuf meta
    On imx platform, only imx8q need AMPHION_TILE modifier
    Add dmabuf modifier will cause decoder extra call S_FMT when
    decide allocation, it is not necessary for other platform.
    So don't always add dmabuf meta

--- ext/wayland/gstwaylandsink.c
+++ ext/wayland/gstwaylandsink.c
@@ -933,8 +933,10 @@ gst_wayland_sink_propose_allocation (GstBaseSink * bsink, GstQuery * query)
     size = vinfo.size;
   }
 
-  drm_modifier = DRM_FORMAT_MOD_AMPHION_TILED;
-  gst_query_add_allocation_dmabuf_meta (query, drm_modifier);
+  if (IS_AMPHION()) {
+    drm_modifier = DRM_FORMAT_MOD_AMPHION_TILED;
+    gst_query_add_allocation_dmabuf_meta (query, drm_modifier);
+  }
 
   if (self->enable_tile && HAS_DCSS ()) {
     drm_modifier = DRM_FORMAT_MOD_VSI_G1_TILED;
