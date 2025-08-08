nxp-imx/gst-plugins-bad

commit ce8c1832e9c2c1c5f64a8a74b0d6dab9b86cb5dc

MMFMWK-9524 wlwindow: fix memory leak when handling last buffer
Last buffer may be rendered but not committed when waylandsink changes
state from PAUSED to READY to clear wlwindow. Then used_by_compositor
flag is always FALSE and this buffer has no chance to be forced free.

For case that v4l2 decoder orphans successfully but waits for last
buffer's force free to stop buffer pool. All decoder's capture buffers
are leaked.

So set used_by_compositor flag for last buffer that rendered but not
committed to avoid memory leak.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9316>

gst-libs/gst/wayland/gstwlwindow.c
@@ -30,6 +30,7 @@
 #include "gstwlwindow.h"
 #include "gstwlutils.h"
 #include "gstimxcommon.h"
+#include "gstwlbuffer_private.h"
 
 #include "fullscreen-shell-unstable-v1-client-protocol.h"
 #include "single-pixel-buffer-v1-client-protocol.h"
@@ -411,8 +412,16 @@ gst_wl_window_finalize (GObject * gobject)
   GstWlWindow *self = GST_WL_WINDOW (gobject);
   GstWlWindowPrivate *priv = gst_wl_window_get_instance_private (self);
 
-  gst_wl_display_callback_destroy (priv->display, &priv->frame_callback);
+  g_mutex_lock (&priv->window_lock);
+  /* last buffer is rendered but not committed, set used_by_compositor
+   * to avoid memory leak */
+  if (priv->commit_callback && priv->next_buffer)
+    gst_wl_buffer_set_used_by_compositor (priv->next_buffer, TRUE);
+
   gst_wl_display_callback_destroy (priv->display, &priv->commit_callback);
+  g_mutex_unlock (&priv->window_lock);
+
+  gst_wl_display_callback_destroy (priv->display, &priv->frame_callback);
   gst_wl_display_object_destroy (priv->display,
       (gpointer *) & priv->xdg_toplevel, (GDestroyNotify) xdg_toplevel_destroy);
   gst_wl_display_object_destroy (priv->display,

gst-libs/gst/wayland/gstwlbuffer.c
@@ -81,6 +81,7 @@
 #endif
 
 #include "gstwlbuffer.h"
+#include "gstwlbuffer_private.h"
 
 #include "linux-explicit-synchronization-unstable-v1-client-protocol.h"
 

gst-libs/gst/wayland/gstwlbuffer.h
@@ -68,7 +68,4 @@ gboolean gst_wl_buffer_get_used_by_compositor (GstWlBuffer * self);
 GST_WL_API
 void gst_wl_buffer_set_buffer_release (GstWlBuffer * self, struct zwp_linux_buffer_release_v1 * buffer_release);
 
-GST_WL_API
-void gst_wl_buffer_set_used_by_compositor (GstWlBuffer * self, gboolean used_by_compositor);
-
 G_END_DECLS

gst-libs/gst/wayland/gstwlbuffer_private.h
@@ -1,0 +1,32 @@
+/* GStreamer Wayland Library
+ *
+ * Copyright 2025 NXP
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __GST_WL_BUFFER_PRIVATE_H__
+#define __GST_WL_BUFFER_PRIVATE_H__
+
+#include <gst/wayland/wayland.h>
+
+G_BEGIN_DECLS
+
+G_GNUC_INTERNAL void gst_wl_buffer_set_used_by_compositor (GstWlBuffer * self, gboolean used_by_compositor);
+
+G_END_DECLS
+
+#endif /* __GST_WL_BUFFER_PRIVATE_H__ */

commit 24947dba4df6663f247d4c5eb89bdb5ad6fa1d5f

MMFMWK-9524 wlwindow: fix dead lock caused by commit ce8c1832e
This is a regression caused by commit ce8c1832e9c2c1c5f64a8a74b0d6dab9b86cb5dc.
Thread 11 holds window_lock and is waiting for sync_mutex, while thread
2 holds sync_mutex and is waiting for window_lock. It causes dead lock.
To fix it, no need to add window lock when destroy commit_callback.

Thread 11 (Thread 0xffff807be160 (LWP 898) "GstPlay"):
\#0  futex_wait (futex_word=0xffff60003e10, expected=2, private=0) at /usr/src/debug/glibc/2.41+git/sysdeps/nptl/futex-internal.h:146
\#1  __GI___lll_lock_wait (futex=futex@entry=0xffff60003e10, private=private@entry=0) at lowlevellock.c:49
\#2  0x0000ffff81113c08 in lll_mutex_lock_optimized (mutex=0xffff60003e10) at pthread_mutex_lock.c:48
\#3  ___pthread_mutex_lock (mutex=0xffff60003e10) at pthread_mutex_lock.c:128
\#4  0x0000ffff808fab94 in gst_wl_display_object_destroy (self=<optimized out>, object=0xffff58002140, destroy_func=0xffff808f9448 <wl_callback_destroy>) at ../gst-libs/gst/wayland/gstwldisplay.c:724
\#5  0x0000ffff808fabd0 in gst_wl_display_callback_destroy (self=<optimized out>, callback=<optimized out>) at ../gst-libs/gst/wayland/gstwldisplay.c:744
\#6  0x0000ffff808fdcd8 in gst_wl_window_finalize (gobject=0xffff58002180) at ../gst-libs/gst/wayland/gstwlwindow.c:421

Thread 2 (Thread 0xffff677ef160 (LWP 970) "GstWlDisplay"):
\#0  syscall () at ../sysdeps/unix/sysv/linux/aarch64/syscall.S:38
\#1  0x0000ffff81320fb4 in ?? () from /usr/lib/libglib-2.0.so.0
\#2  0x0000ffff808fe42c in commit_callback (data=0xffff58002180, callback=<optimized out>, serial=<optimized out>) at ../gst-libs/gst/wayland/gstwlwindow.c:976
\#3  0x0000ffff80e768d4 in ?? () from /usr/lib/libffi.so.8
\#4  0x0000ffff80e75fdc in ?? () from /usr/lib/libffi.so.8
\#5  0x0000ffff8089a604 in ?? () from /usr/lib/libwayland-client.so.0
\#6  0x0000ffff80896430 in ?? () from /usr/lib/libwayland-client.so.0

gst-libs/gst/wayland/gstwlwindow.c
@@ -417,11 +417,10 @@ gst_wl_window_finalize (GObject * gobject)
    * to avoid memory leak */
   if (priv->commit_callback && priv->next_buffer)
     gst_wl_buffer_set_used_by_compositor (priv->next_buffer, TRUE);
-
-  gst_wl_display_callback_destroy (priv->display, &priv->commit_callback);
   g_mutex_unlock (&priv->window_lock);
 
   gst_wl_display_callback_destroy (priv->display, &priv->frame_callback);
+  gst_wl_display_callback_destroy (priv->display, &priv->commit_callback);
   gst_wl_display_object_destroy (priv->display,
       (gpointer *) & priv->xdg_toplevel, (GDestroyNotify) xdg_toplevel_destroy);
   gst_wl_display_object_destroy (priv->display,